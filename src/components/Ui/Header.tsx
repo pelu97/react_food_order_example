deb82d716063a4d16c3226bbfbdbf410dc004481
pelu97 <pedrolpandrade@gmail.com>
2022-06-14 11:50:41 -0300

    Added Card and basic Header UI components

---
+++ src/components/Ui/Header.tsx
@@ -0,0 +1,15 @@
+
+import classes from "./Header.module.css";
+
+function Header(){
+    return(
+        <div className={classes.header}>
+            <h2>Delivery Name Placeholder</h2>
+            <div>
+                Cart Button Placeholder
+            </div>
+        </div>
+    );
+}
+
+export default Header;
