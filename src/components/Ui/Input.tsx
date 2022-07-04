bcb5c755b1b8177474fdaf40c6fc80befc4c90a0
pelu97 <pedrolpandrade@gmail.com>
2022-07-04 10:10:56 -0300

    Added support for alt and invalid css classes and for the onBlur event

--- src/components/Ui/Input.tsx
+++ src/components/Ui/Input.tsx
@@ -10,16 +10,46 @@ interface InputProps{
     value?: string,
     min?: string,
     max?: string
-    onChange: (value: string) => void
+    onChange: (value: string) => void,
+    onBlur?: () => void,
+    overrideStyle?: string,
+    isValid?: boolean
 }
 
 function Input(props: InputProps){
+
+    let inputClass = classes.input;
+
+    if(props.overrideStyle !== undefined){
+        if(props.overrideStyle === "alternative"){
+            inputClass = classes["input--alt"]
+        }
+    }
+
+    // isValid is defined and it's false
+    if(props.isValid !== undefined && !props.isValid){
+        inputClass = `${inputClass} ${classes.invalid}`;
+    }
+
     return (
-        <div className={classes.input}>
+        <div className={inputClass}>
             <label htmlFor={props.id}>{props.label}</label>
-            <input id={props.id} type={props.type} value={props.value} min={props.min} max={props.max} onChange={(event) => {
-                props.onChange(event.target.value);
-            }}/>
+            <input
+                id={props.id}
+                type={props.type}
+                value={props.value}
+                min={props.min}
+                max={props.max}
+                onChange={(event) => {
+                    props.onChange(event.target.value);
+                }}
+                onBlur={(_) => {
+                    if(props.onBlur){
+                        console.log("blur");
+                        props.onBlur();
+                    }
+                }}
+            />
         </div>
     );
 }
